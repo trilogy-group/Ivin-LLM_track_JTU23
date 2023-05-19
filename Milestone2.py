22d46ce210786b2ea8aa231e2925412b46875b6b
Ivin KURIAKOSE <ivinkuriakose@Ivins-MacBook-Air.local>
2023-05-20 02:24:20 +0530

    second version of Milestone 2 -- Added prompt template

--- Milestone2.py
+++ Milestone2.py
@@ -3,9 +3,15 @@ import openai
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
-llm = OpenAI(temperature=0)
+llm = OpenAI(temperature=0.9)
+
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="corrects all the math calculations in following text - {product}?",
+)
 
 
-text = "corrects all the math calculations in following text - "
-text+="If a recipe requires 2 cups of flour and I want to make 4 batches, I will need a total of 6.5 cups of flour."
-print(llm(text))
+# Insert your text within the double quotes
+query1 = prompt.format(product="If a recipe requires 2 cups of flour and I want to make 4 batches, I will need a total of 6.5 cups of flour.")
+
+print(llm(query1))

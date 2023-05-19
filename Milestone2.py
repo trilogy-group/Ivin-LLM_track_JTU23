319708af6e05db6470d5fe4e147c6d77a92354be
Ivin KURIAKOSE <ivinkuriakose@Ivins-MacBook-Air.local>
2023-05-20 02:26:33 +0530

    final version of milestone 2  --  output commented below the code.

--- Milestone2.py
+++ Milestone2.py
@@ -10,8 +10,26 @@ prompt = PromptTemplate(
     template="corrects all the math calculations in following text - {product}?",
 )
 
-
-# Insert your text within the double quotes
 query1 = prompt.format(product="If a recipe requires 2 cups of flour and I want to make 4 batches, I will need a total of 6.5 cups of flour.")
+query2 = prompt.format(product="If each cup have 20 ml of beer in it, then if i drink 7 cups then i would have ingested 140ml of beer.")
+query3 = prompt.format(product="If a store offers a discount of 25% on a $80 item, the discounted price will be $40.")
 
-print(llm(query1))
+print(llm(query1),"\n\n")
+print(llm(query2),"\n\n")
+print(llm(query3))
+
+
+# Output
+
+
+# If a recipe requires 2 cups of flour and I want to make 4 batches, I will need a total of 8 cups of flour. 
+
+
+
+
+# If each cup has 20 mL of beer in it, then if I drink 7 cups, I would have ingested 140 mL of beer. 
+
+
+
+
+# If a store offers a discount of 25% on a $80 item, the discounted price will be $60.
